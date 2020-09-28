7cef0da981e425b367459b7fe06dc51f24e4eefd
oiraven <71995821+oiraven@users.noreply.github.com>
2020-09-27 23:56:06 -0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+ 
+
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
+});
+
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
+});
+
+ 
+
+// THIS  MUST  BE  THIS  WAY
+
+client.login(process.env.hksEY7l1z-58C2eEwfewx-YJOvRLgX_d);
